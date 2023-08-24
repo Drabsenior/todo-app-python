47892fa784e07c5f48276980e713a88c332c755a
Drabsenior <drabsenior@gmail.com>
2023-08-24 14:17:13 +0300

    implement the edit button

--- gui.py
+++ gui.py
@@ -2,13 +2,17 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text('Type in a todo: ')
-input_box = sg.Input(tooltip='Enter todo' , key='todo' )
+input_box = sg.Input(tooltip='Enter todo', key='todo' )
 add_button = sg.Button('Add')
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]] , font=('Helvetica',16))
+list_box = sg.Listbox(values=functions.get_todos() , key='todos', size=[45, 10], enable_events=True)
+edit_button = sg.Button('Edit')
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button], [list_box,edit_button]], font=('Helvetica' ,16))
 
 
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
     match event:
         case 'Add':
             todos = functions.get_todos()
@@ -16,6 +20,19 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            print('hello')
+            print(values['todo'])
+            print(values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
+        case 'Edit':
+            todos = functions.get_todos()
+            edit_input = values['todos'][0]
+            index = todos.index(edit_input)
+            todos[index] = values['todo'] + '\n'
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case sg.WIN_CLOSED:
             break
 
